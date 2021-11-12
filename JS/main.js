4002965fc5a01ae570abea2dfedeb92cc186fc90
MrFatkhulloh <eternallfame@gmail.com>
2021-11-13 02:43:39 +0500

    complete 9-lesson

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,8 @@
+/*
 var ON_FOOT = 3.6;
 var BY_BICYCLE = 20.1;
 var BY_CAR = 70;
 var BY_PLANE = 800;
-
-var minON_FOOT = ON_FOOT/60;
-var minBY_BICYCLE = BY_BICYCLE/60;
-var minBY_CAR = BY_CAR/60;
-var minBY_PLANE = BY_PLANE/60;
 
 var elFoot = document.querySelector("#foot");
 var elBicycle = document.querySelector("#bicycle");
@@ -18,30 +14,81 @@ var elForm = document.querySelector("#kilometer");
 elForm.addEventListener("submit", function(evt) {
     evt.preventDefault();
     var elDistance = document.querySelector("#distance");
-    var walkmin = elDistance.value/ON_FOOT;
-    var ridemin = elDistance.value/BY_BICYCLE;
-    var drivemin = elDistance.value/BY_CAR;
-    var flymin = elDistance.value/BY_PLANE;
+    var walk = elDistance.value/ON_FOOT;
+    var ride = elDistance.value/BY_BICYCLE;
+    var drive = elDistance.value/BY_CAR;
+    var fly = elDistance.value/BY_PLANE;
+
+    if(walk > 23) {
+        elFoot.textContent = `${Math.floor(walk/24)} days ${Math.floor(walk%24)} hours ${ Math.round(Math.floor(walk) - walk%24)} minutes`;
+    }else if (walk < 24 && walk > 1) {
+        elFoot.textContent = `${Math.floor(walk%24)} hour(s) ${ Math.round(Math.floor(walk) - walk%24)} minutes`;
+    }else {
+        elFoot.textContent = `${ Math.round(Math.floor(walk) - walk%24)} minutes`;        
+    }
+
+
+    elFoot.textContent = `${Math.round(walk)} h`;
+    elBicycle.textContent = `${Math.round(ride)} h`;
+    elCar.textContent = `${Math.round(drive)} h`;
+    elPlane.textContent = `${Math.round(fly)} h`;
     
-    elFoot.textContent = `${Math.round(walkmin)} h`;
-    elBicycle.textContent = `${Math.round(ridemin)} h`;
-    elCar.textContent = `${Math.round(drivemin)} h`;
-    elPlane.textContent = `${Math.round(flymin)} h`;
+
     
-//     if((elFoot.textContent - 0) > 59) {
-//         // elFoot.textContent = `${elFoot.textContent/60} h ${elFoot.textContent} min`;
-//         // elBicycle.textContent = `${elBicycle.textContent/60} h ${elBicycle.textContent} min`;
-//         // elCar.textContent = `${elCar.textContent/60} h ${elCar.textContent} min`;
-//         // elPlane.textContent = `${elPlane.textContent/60} h ${elPlane.textContent} min`;
-//         console.log("TRUE");
-//     }else {
-//         // elFoot.textContent = `${Math.round(walkmin)} min`;
-//         // elBicycle.textContent = `${Math.round(ridemin)} min`;
-//         // elCar.textContent = `${Math.round(drivemin)} min`;
-//         // elPlane.textContent = `${Math.round(flymin)} min`;
-//         console.log("FALSE");
-//     }
-// 
+    console.log(elFoot.textContent);
 }
 )
+*/
 
+var elForm = document.querySelector("#input-form");
+var elInputDistance = document.querySelector("#inputdistance");
+var elSubmitBtn = document.querySelector("#submit_btn");
+
+var elFoot = document.querySelector("#foot");
+var elBicycle = document.querySelector("#bike");
+var elCar = document.querySelector("#car");
+var elPlane = document.querySelector("#plane");
+
+var SPEED_WALK = 3.6;
+var SPEED_BY_BIKE = 20.1;
+var SPEED_BY_CAR = 70;
+var SPEED_BY_PLANE = 800;
+
+function spendingTime(distance, speed) {
+    var hours = distance / speed;
+    var minutes = (hours - Math.floor(hours)) * 60;
+    var minutesOnly = Math.floor(minutes);
+    var seconds = (minutes - minutesOnly) * 60;
+    var secondsOnly = Math.floor(seconds);
+    var days = hours / 24;
+    var daysOnly = Math.floor(days);
+    var hoursOnly = Math.floor(hours - daysOnly*24);
+
+    hoursShow = hoursOnly ? `${hoursOnly} hours` : "";
+    minutesShow = minutesOnly ? `${minutesOnly} minutes` : "";
+    secondsShow = secondsOnly ? `${secondsOnly} seconds` : "";
+    daysShow = daysOnly ? `${daysOnly} days` : "";
+
+    return `${daysShow}  ${hoursShow}  ${minutesShow}  ${secondsShow}`
+}
+
+//_______________________________________________
+
+elForm.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    var inputDistance = Number(elInputDistance.value.trim());
+
+    if(inputDistance <= 0) {
+        alert(`No'ldan katta raqam kiriting`);
+        return console.log(`No'ldan katta raqam kiriting`);
+    }else if(isNaN(inputDistance)) {
+        alert(`Raqam kiriting`);
+        return console.log(`Raqam kiriting`);
+    }else
+
+    console.log(true);
+    elFoot.textContent = spendingTime(inputDistance, SPEED_WALK);
+    elBicycle.textContent = spendingTime(inputDistance, SPEED_BY_BIKE);
+    elCar.textContent = spendingTime(inputDistance, SPEED_BY_CAR);
+    elPlane.textContent = spendingTime(inputDistance, SPEED_BY_PLANE);
+})
